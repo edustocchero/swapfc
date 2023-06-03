c7933cf0169e4709cf56ad5c14ec8dcd5c720ea7
edustocchero <edurevsky@gmail.com>
2023-06-03 13:24:48 -0300

    add main execution

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 mod io;
 
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 || args.len() > 3 {
+        todo!();
+    }
+
+    let source = &args[1];
+    let dest = &args[2];
+
+    if source.eq(dest) {
+        todo!();
+    }
+
+    io::swap_file_content(&source, &dest).unwrap_or_else(|err| {
+        eprintln!("Could not execute swapfc: {:?}", err);
+        std::process::exit(1);
+    });
+
+    std::process::exit(0);
 }
